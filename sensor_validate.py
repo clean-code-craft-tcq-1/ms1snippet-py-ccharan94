beb0ae8c213b5ce8f525f2945e8dd7a29b381c6b
ccharan94 <77478060+ccharan94@users.noreply.github.com>
2021-03-25 00:29:31 +0530

    Update sensor_validate.py

--- sensor_validate.py
+++ sensor_validate.py
@@ -1,19 +1,25 @@
+spike_threshold_limit         = {
+                                  'soc'      :  0.05 ,
+                                  'current'  :  0.1  ,
+                                }
 
-def  _give_me_a_good_name(value, nextValue, maxDelta):
+def isAbnormalSpike(value, nextValue, maxDelta):
   if nextValue - value > maxDelta:
     return False
   return True
 
-def validate_soc_reading(values):
+def hasAbnormalSpike(values,criteria):
   last_but_one_reading = len(values) - 1
   for i in range(last_but_one_reading):
-    if(not _give_me_a_good_name(values[i], values[i + 1], 0.05)):
+    if(not isAbnormalSpike(values[i], values[i + 1], spike_threshold_limit[criteria])):
       return False
-  return True
 
-def validate_current_reading(values):
-  last_but_one_reading = len(values) - 1
-  for i in range(last_but_one_reading):
-    if(not _give_me_a_good_name(values[i], values[i + 1], 0.1)):
-      return False
-  return True
+def isValidValueList(values):
+    if isinstance(values, list) and len(values) != 0:
+        return True
+    return False
+
+def isValidReading(values,criteria):
+    if isValidValueList(values) and not hasAbnormalSpike(values,criteria):
+        return True
+    return False
