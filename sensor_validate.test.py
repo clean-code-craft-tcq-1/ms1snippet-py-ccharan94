c1dc87e0974cc9420adcfa055b06b548bdd15fdb
ccharan94 <77478060+ccharan94@users.noreply.github.com>
2021-03-25 13:16:34 +0530

    Update sensor_validate.test.py

--- sensor_validate.test.py
+++ sensor_validate.test.py
@@ -2,12 +2,32 @@ import unittest
 import sensor_validate
 
 class SensorValidatorTest(unittest.TestCase):
-  def test_reports_error_when_soc_jumps(self):
+  def test_error_when_soc_jumps(self):
     self.assertFalse(
       sensor_validate.isValidReading([0.0, 0.01, 0.5, 0.51],'soc')
     )
+    
+  def test_error_when_no_readings(self):
+    self.assertFalse(
+      sensor_validate.isValidReading([],'soc')
+    )
+    
+  def test_error_when_sensor_off(self):
+    self.assertFalse(
+      sensor_validate.isValidReading(None,'soc')
+    )
+    
+  def test_noerror_when_soc_readings_OK(self):
+    self.assertTrue(
+      sensor_validate.isValidReading([0.0, 0.01, 0.02, 0.03],'soc')
+    )
+    
+  def test_noerror_when_current_readings_OK(self):
+    self.assertTrue(
+      sensor_validate.isValidReading([0.03, 0.03, 0.03, 0.03],'current')
+    )
   
-  def test_reports_error_when_current_jumps(self):
+  def test_error_when_current_jumps(self):
     self.assertFalse(
       sensor_validate.isValidReading([0.03, 0.03, 0.03, 0.33],'current')
     )
